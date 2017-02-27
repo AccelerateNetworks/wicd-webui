498197016f5e28e01850c7cafb104bea56c64cf7
Finn <finn@finn.io>
2017-02-26 17:28:49 -0800

    Clean up newly external JS

--- static/js/wicd.js
+++ static/js/wicd.js
@@ -15,7 +15,7 @@ $.fn.serializeObject = function() {
 };
 
 var statusModal = (function () {
-  var mainDiv = $('<div class="modal fade" id="statusModal" tabindex="-1" role="dialog" aria-labelledby="statusModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="statusModalLabel"></h4></div><div class="modal-body" id="statusModalText"></div></div></div></div>')
+  var mainDiv = $('<div class="modal fade" id="statusModal" tabindex="-1" role="dialog" aria-labelledby="statusModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="statusModalLabel"></h4></div><div class="modal-body" id="statusModalText"></div></div></div></div>');
   var textDiv = $('#statusModalText', mainDiv);
   var labelDiv = $('#statusModalLabel', mainDiv);
   var closeButton = $('.close', mainDiv);
@@ -45,111 +45,102 @@ function refresh_networks(data) {
 
   $('#network-table>tbody').remove();
   var tbody = $('<tbody/>');
-  var known
-  $.each( data['data'], function( key, val ) {
-    if (val["known"]) known = "<span class=\"glyphicon glyphicon-ok\">";
+  var known;
+  $.each( data.data, function( key, val ) {
+    if (val.known) {
+      known = "<span class=\"glyphicon glyphicon-ok\">";
+    }
     else known = "";
-    $("<tr/>", { html: "\
-      <td>" + val["essid"] + "</td>\
-      <td>" + val["encryption"] + known + "</td>\
-      <td>" + val["quality"] + "%</td>\
-      <td>\
-      <a href=\"javascript:connect_to_network(" + val["network_id"] + ");\">\
-      <span class=\"glyphicon glyphicon-log-in\">\
-      </a>\
-      &nbsp;\
-      <a href=\"javascript:configure_network(" + val["network_id"] + ");\">\
-      <span class=\"glyphicon glyphicon-cog\">\
-      </a>\
-      </td>\
-      "}).appendTo(tbody);
+    var row = $("<tr />");
+    row.append($("<td>" + val.essid + "</td>"));
+    row.append($("<td>" + val.encryption + known + "</td>"));
+    row.append($("<td>" + val.quality + "%</td>"));
+    row.append($("<td><a href=\"javascript:connect_to_network(" + val.network_id + ");\"><span class=\"glyphicon glyphicon-log-in\"></a>&nbsp;<a href=\"javascript:configure_network(" + val.network_id + ");\"><span class=\"glyphicon glyphicon-cog\"></a></td>"));
+    row.appendTo(tbody);
   });
   tbody.appendTo("#network-table");
   statusModal.hide();
-};
+}
 
 function list_networks() {
   statusModal.show();
-  statusModal.text('Listing networks...')
+  statusModal.text('Listing networks...');
   $.getJSON( "list", refresh_networks );
-};
+}
 
 function scan_networks() {
   statusModal.show();
-  statusModal.text('Scanning networks...')
+  statusModal.text('Scanning networks...');
   $.getJSON( "scan", refresh_networks );
-};
+}
 
 function connect_to_network(network_id) {
   statusModal.show();
-  statusModal.text('Connecting...')
+  statusModal.text('Connecting...');
   $.getJSON( "connect/" + network_id, function(data) {
-    statusModal.label('Please wait (' + data['data'] + ')');
-    statusModal.text('Connecting to ' + data['data'] + '...');
+    statusModal.label('Please wait (' + data.data + ')');
+    statusModal.text('Connecting to ' + data.data + '...');
     watch_connection_status();
   } );
-};
+}
 
 function watch_connection_status() {
   $.getJSON( "status", function(data) {
-    connecting = data['data'][0];
-    message = data['data'][2];
+    connecting = data.data[0];
+    message = data.data[2];
     statusModal.text(message);
     if (connecting) setTimeout(watch_connection_status, 1000);
     else {
       statusModal.closeable();
       current_network();
-    };
+    }
   });
 }
 
 function disconnect_network() {
   statusModal.show();
-  statusModal.text('Disconnecting...')
+  statusModal.text('Disconnecting...');
   $.getJSON( "disconnect", function(data) {
     $('#current_network>p').text("Currently not connected to any network");
     statusModal.hide();
   } );
-};
+}
 
 function current_network() {
   $.getJSON( "current", function(data) {
     $('#current_network>p').remove();
     var newp = $('<p/>');
-    if ($.isEmptyObject(data['data']))
+    if ($.isEmptyObject(data.data))
       newp.text("Currently not connected to any network");
     else {
-      newp.text('Connected to ')
-      newp.append($('<i/>').text('"' + data['data']['network'] + '"'));
+      newp.text('Connected to ');
+      newp.append($('<i/>').text('"' + data.data.network + '"'));
       newp.append(document.createTextNode(' at '));
-      newp.append($('<i/>').text(data['data']['quality'] + '%'));
+      newp.append($('<i/>').text(data.data.quality + '%'));
       newp.append(document.createTextNode(' (IP: '));
-        newp.append($('<i/>').text(data['data']['ip']));
+        newp.append($('<i/>').text(datadata.ip));
         newp.append(document.createTextNode(')'));
-        newp.append('\
-          <a href=\"javascript:disconnect_network();\">\
-          <span class=\"glyphicon glyphicon-log-out\">\
-          </a>');
+        newp.append('<a href=\"javascript:disconnect_network();\"><span class=\"glyphicon glyphicon-log-out\"></a>');
       }
       newp.appendTo("#current_network");
     });
-};
+}
 
 function configure_network(network_id) {
   statusModal.show();
-  statusModal.text('Listing networks...')
+  statusModal.text('Listing networks...');
   $.getJSON( "details/" + network_id, function(data) {
     statusModal.hide();
-    var info = data['data'];
+    var info = data.data;
     var modal = $('#configModal');
-    $('#configModalLabel',modal).text('Configure ' + info['bssid']);
-    $('#configModalESSID',modal).val(info['essid']);
-    $('#configModalEncryptionMethod',modal).text('Encryption is ' + info['encryption']);
-    $('#configModalNetworkId',modal).val(network_id);
+    $('#configModalLabel', modal).text('Configure ' + info.bssid);
+    $('#configModalESSID', modal).val(info.essid);
+    $('#configModalEncryptionMethod', modal).text('Encryption is ' + info.encryption);
+    $('#configModalNetworkId', modal).val(network_id);
 
-    if (info['encryption'] == 'Off') {
-      $('#configModalPassword',modal).prop('disabled', true);
-      $('#configModalEncryption',modal).prop('disabled', true);
+    if (info.encryption == 'Off') {
+      $('#configModalPassword', modal).prop('disabled', true);
+      $('#configModalEncryption', modal).prop('disabled', true);
     }
     else {
       $('#configModalPassword',modal).prop('disabled', false);
@@ -157,7 +148,7 @@ function configure_network(network_id) {
     }
     $('.selectpicker').selectpicker('refresh');
 
-    if (info['essid'] === '') {
+    if (info.essid === '') {
       $('#configModalESSID',modal).prop('disabled', false);
     }
     else {
