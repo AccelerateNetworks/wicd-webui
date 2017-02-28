0b85cfed170c6952844ef31550426d9b43ddb028
dan <dan@callpipe.com>
2017-02-28 01:05:01 -0800

    Broken stuff, networks don't display.

--- static/js/wicd.js
+++ static/js/wicd.js
@@ -69,7 +69,7 @@ function qualityBar(q) {
 
 function actionButtons(id) {
   var group = $("<div>").addClass('btn-group');
-  var connect = $("<a>").data('id', id).click(connect_to_network).text("Connect ").addClass('btn btn-success btn-sm').append($("<span>").addClass('glyphicon glyphicon-log-in'));
+  var connect = $("<a>").data('id', id).click(network_status_precheck).text("Connect ").addClass('btn btn-success btn-sm').append($("<span>").addClass('glyphicon glyphicon-log-in'));
   var configure = $("<a>").data('id', id).click(configure_network).text("Configure ").addClass('btn btn-info btn-sm').append($("<span>").addClass('glyphicon glyphicon-cog'));
   return group.append(connect).append(configure);
 }
@@ -106,14 +106,26 @@ function scan_networks() {
   $.getJSON( "scan", refresh_networks );
 }
 
-function connect_to_network() {
-  var network_id = $(this).data('id');
-  showAlert('Connecting...', 'info');
-  $.getJSON( "connect/" + network_id, function(data) {
-    showAlert('Please wait (' + data.data + ')', 'info');
-    showAlert('Connecting to ' + data.data, 'info');
-    watch_connection_status();
-  } );
+function network_status_precheck(){
+  $.getJSON("current", connect_to_network($(this).data('id')));
+}
+
+function connect_to_network(network_id) {
+  return function(current) {
+    console.log("Preparing to connect to network...", network_id, current);
+    if(typeof current.network === undefined) {
+      showAlert('Connecting...', 'info');
+      $.getJSON("connect/" + network_id, function(data) {
+        showAlert('Please wait (' + data.data + ')', 'info');
+        showAlert('Connecting to ' + data.data, 'info');
+        watch_connection_status();
+      });
+    } else {
+        statusModal.show();
+        statusModal.text('Are you sure you want to disconnect from ' + current.network);
+
+    }
+  }
 }
 
 function watch_connection_status() {
